3c0e2e26d39323b77352112a26886fab9ada2f36
Maddie Rogers <madisonrogers@mymail.mines.edu>
2017-03-13 16:33:25 -0600

    Added a function that said an activity

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -10,8 +10,21 @@ public class TheMotivator {
 			System.out.println("Well, what can I say?");
 	}
 	
+	public void activity(int num)
+	{
+		if (num == 100)
+			System.out.println("You ran a mile");
+		else if (num > 90)
+			System.out.println("You walked a mile");
+		else if (num > 60)
+			System.out.println("You got out of bed");
+		else
+			System.out.println("You stayed in bed all day");
+	}
+	
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
+		tm.activity(60);
 		tm.feedback(60);
 	}
 }
